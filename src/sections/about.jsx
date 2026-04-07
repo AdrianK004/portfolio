fbda14efbfed4c6f15db3e458cea594eec7b91a2
Adrian Krasniqi <adriankrasniqi1992@gmail.com>
2026-04-08 01:02:18 +0200

    About Page & Download CV

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import cvFile from '../assets/AdrianKrasniqiCV.pdf';
 
 const About = () => {
   const [skills, setSkills] = useState([
@@ -96,6 +97,7 @@ const About = () => {
               <p className="text-gray-600 dark:text-gray-300 font-semibold">
                 CONNECT WITH ME &gt;
               </p>
+
               <div className="flex gap-3">
                 {/* LinkedIN */}
                 <a href="https://www.linkedin.com/in/adrian-krasniqi-170511400/" className="text-gray-600 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400 transition-colors duration-200">
@@ -116,10 +118,18 @@ const About = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
                 </a>
               </div>
+
+              <a 
+                href={cvFile}
+                download="AdrianKrasniqiCV.pdf"
+                className="bg-blue-600/10 backdrop-blur-sm hover:bg-blue-600/20 text-blue-200 px-4 py-2 rounded-xl flex items-center text-sm font-bold border border-blue-400/50"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="4 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-download"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" /><path d="M12 17v-6" /><path d="M9.5 14.5l2.5 2.5l2.5 -2.5" /></svg>
+                Download CV
+              </a>
             </div>
           </div>
 
-          {/* Kolona e djathtë - Skill Bars */}
           <div className="space-y-6">
             {skills.map((skill, idx) => (
               <div key={idx}>
